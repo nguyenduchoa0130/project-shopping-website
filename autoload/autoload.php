d2fb466fa7b6dfa8175d7a5c7385d94dee40c40f
duyhoang3820 <duyhoang3820@gmail.com>
2020-12-21 15:34:17 +0700

    update auttoload

--- autoload/autoload.php
+++ autoload/autoload.php
@@ -1,5 +1,12 @@
 <?php
+    header ('Content-Type: text/html; charset=UTF-8'); 
     require_once __DIR__. "/../lib/database.php";
     require_once __DIR__. "/../lib/function.php";
-    require_once __DIR__. "/../lib/var-global.php";
+    require_once __DIR__. "/../lib/category.php";
+    require_once __DIR__. "/../lib/product.php";
+    require_once __DIR__. "/../lib/image.php";
+    require_once __DIR__. "/../lib/order.php";
+    session_start();
+    $database = new Database();
+    $_SESSION["username"] = null;
 ?>
