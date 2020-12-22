aa3437c7cf050a0c466dc3bd7dddc75aeca329e5
Nguyen Duc Hoa <66864892+nguyenduchoa0130@users.noreply.github.com>
2020-12-22 20:10:23 +0700

    Modify database: change mysqli to pdo

--- lib/database.php
+++ lib/database.php
@@ -1,138 +1,54 @@
 <?php
-
+require_once __DIR__ . "/function.php";
 class Database
 {
-    /**
-     * Khai báo biến kết nối
-     * @var [type]
-     */
-    public $connect;
-
-    public function __construct()
+    public $connection = null;
+    public function __construct($server, $db, $username, $password)
     {
-        $this->connect = mysqli_connect("localhost", "root", "", "db_shopping") or die();
-        mysqli_set_charset($this->connect, "utf8");
+        $this->connection = new PDO("mysql:host=$server;dbname=$db;charset=utf8", $username, $password);
+        $this->connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     }
-
-
-    /**
-     * [insert description] hàm insert 
-     * @param  $table
-     * @param  array  $data  
-     * @return integer
-     */
+    public function disconnect()
+    {
+        $this->connection = null;
+    }
+    public function fetchSql($sql)
+    {
+        $query = $this->connection->prepare($sql);
+        $query->execute();
+        return $query->fetchAll();
+    }
+    public function fetchDataAll($table)
+    {
+        $query = $this->connection->prepare("SELECT * FROM `{$table}`");
+        $query->execute();
+        $data = $query->fetchAll();
+        return $data;
+    }
+    public function fetchDataById($table, $id_name, $val)
+    {
+        $query = $this->connection->prepare("SELECT * FROM `{$table}` WHERE `{$id_name}` = {$val}");
+        $query->execute();
+        $data = $query->fetch(PDO::FETCH_ASSOC);
+        return $data;
+    }
     public function insert($table, array $data)
     {
-        
-        $sql = "INSERT INTO {$table} ";
-        $columns = implode(',', array_keys($data));
-        $values  = "";
-        $sql .= '(' . $columns . ')';
-        foreach ($data as $field => $value) {
-            if (is_string($value)) {
-                $values .= "'" . mysqli_real_escape_string($this->connect, $value) . "',";
-            } else {
-                $values .= mysqli_real_escape_string($this->connect, $value) . ',';
-            }
-        }
-        $values = substr($values, 0, -1);
-        $sql .= " VALUES (" . $values . ')';
-        // _debug($sql);die;
-        mysqli_query($this->connect, $sql) or die("Lỗi  query  insert ----" . mysqli_error($this->connect));
-        return mysqli_insert_id($this->connect);
+        $sqlStr = createInsertSql($table, $data);
+        return $sqlStr;
+        $query = $this->connection->prepare($sqlStr);
+        return $query->execute();
     }
-
-    public function updateview($sql)
+    public function update($table, $id_name, $val, $data)
     {
-        $result = mysqli_query($this->connect, $sql)  or die("Lỗi update view " . mysqli_error($this->connect));
-        return mysqli_affected_rows($this->connect);
+        $sqlStr = createUpdateSql($table, $id_name, $val, $data);
+        $query = $this->connection->prepare($sqlStr);
+        return $query->execute();
     }
-    public function countTable($table)
+    public function delete($table, $id_name, $val)
     {
-        $sql = "SELECT id FROM  {$table}";
-        $result = mysqli_query($this->connect, $sql) or die("Lỗi Truy Vấn countTable----" . mysqli_error($this->connect));
-        $num = mysqli_num_rows($result);
-        return $num;
+        $sqlStr = "DELETE FROM `{$table}` WHERE `{$id_name}` = {$val}";
+        $query = $this->connection->prepare($sqlStr);
+        return $query->execute();
     }
-
-
-    /**
-     * [delete description] hàm delete
-     * @param  $table      [description]
-     * @param  array  $conditions [description]
-     * @return integer             [description]
-     */
-    public function delete($table,  $id)
-    {
-        $sql = "DELETE FROM {$table} WHERE id = $id ";
-
-        mysqli_query($this->connect, $sql) or die(" Lỗi Truy Vấn delete   --- " . mysqli_error($this->connect));
-        return mysqli_affected_rows($this->connect);
-    }
-
-    /**
-     * delete array 
-     */
-
-    public function deletewhere($table, $data = array())
-    {
-        foreach ($data as $id) {
-            $id = intval($id);
-            $sql = "DELETE FROM {$table} WHERE id = $id ";
-            mysqli_query($this->connect, $sql) or die(" Lỗi Truy Vấn delete   --- " . mysqli_error($this->connect));
-        }
-        return true;
-    }
-
-    public function fetchsql($sql)
-    {
-        $result = mysqli_query($this->connect, $sql) or die("Lỗi  truy vấn sql " . mysqli_error($this->connect));
-        $data = [];
-        if ($result) {
-            while ($num = mysqli_fetch_assoc($result)) {
-                $data[] = $num;
-            }
-        }
-        return $data;
-    }
-
-    public function fetchID($table, $id)
-    {
-        $sql = "SELECT * FROM {$table} WHERE id = $id ";
-        $result = mysqli_query($this->connect, $sql) or die("Lỗi  truy vấn fetchID " . mysqli_error($this->connect));
-        return mysqli_fetch_assoc($result);
-    }
-
-    public function fetchOne($table, $query)
-    {
-        $sql  = "SELECT * FROM {$table} WHERE ";
-        $sql .= $query;
-        $sql .= "LIMIT 1";
-        $result = mysqli_query($this->connect, $sql) or die("Lỗi  truy vấn fetchOne " . mysqli_error($this->connect));
-        return mysqli_fetch_assoc($result);
-    }
-
-    public function deletesql($table,  $sql)
-    {
-        $sql = "DELETE FROM {$table} WHERE " . $sql;
-        mysqli_query($this->connect, $sql) or die(" Lỗi Truy Vấn delete   --- " . mysqli_error($this->connect));
-        return mysqli_affected_rows($this->connect);
-    }
-
-
-    public function fetchAll($table)
-    {
-        $sql = "SELECT * FROM {$table} WHERE 1";
-        $result = mysqli_query($this->connect, $sql) or die("Lỗi Truy Vấn fetchAll " . mysqli_error($this->connect));
-        $data = [];
-        if ($result) {
-            while ($num = mysqli_fetch_assoc($result)) {
-                $data[] = $num;
-            }
-        }
-        return $data;
-    }
-
 }
-
-?>
