a6a0ae76e789f313b668d90eda087d8fa15321dc
Nguyen Duc Hoa <66864892+nguyenduchoa0130@users.noreply.github.com>
2021-01-02 23:40:50 +0700

    Update add-product.php
    change handle add product

--- admin/modules/product/add-product.php
+++ admin/modules/product/add-product.php
@@ -1,15 +1,29 @@
 <?php
 require_once __DIR__ . "/../../../autoload/autoload.php";
-if ($_POST["values"]) {
-    $data = $_POST["values"];
-    $props = handleDataForm($data);
-    array_pop($props);
-    $result = $database->insert("tbl_product", $props);
-    if($result){
-        echo "<h4 class='modal-title w-100 text-success' id='myModalLabel'>Thêm Thành Công</h4>";
-    }else{
-        echo "<h4 class='modal-title w-100 text-danger' id='myModalLabel'>Thêm Thất Bại</h4>";
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $props = array(
+        "name_product" => $_POST["name_product"],
+        "price" => $_POST["price"],
+        "quantity" => $_POST["quantity"],
+        "produced_at" => $_POST["produced_at"],
+        "description" => $_POST["description"],
+        "id_category" => $_POST["id_category"]
+    );
+
+    try {
+        $id = $database->insert("tbl_product", $props);
+        $imgs = array();
+        for ($i = 1; $i < 4; $i++) {
+            $img = convertImageToBase64("image" . $i);
+            if ($img) {
+                array_push($imgs, ["name" => $img["name"], "image" => $img["image"], "id_product" => $id]);
+            }
+        }
+        foreach($imgs as $img){
+            $database->insert("tbl_image_product", $img);
+        }
+    } catch (PDOException $e) {
+        echo $e->getMessage();
     }
-} else {
-    echo "Có Lỗi Đã Xảy Ra !!!";
+    exit();
 }
