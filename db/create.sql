6bd63f36a617db1703b0b6fbc6c34dd514efa80d
Nguyen Duc Hoa <66864892+nguyenduchoa0130@users.noreply.github.com>
2021-01-10 10:04:57 +0700

    Update

--- db/create.sql
+++ db/create.sql
@@ -2,10 +2,10 @@
 -- version 5.0.2
 -- https://www.phpmyadmin.net/
 --
--- Máy chủ: 127.0.0.1:3306
--- Thời gian đã tạo: Th1 02, 2021 lúc 05:44 AM
--- Phiên bản máy phục vụ: 5.7.31
--- Phiên bản PHP: 7.3.21
+-- Host: 127.0.0.1:3306
+-- Generation Time: Jan 10, 2021 at 02:41 AM
+-- Server version: 5.7.31
+-- PHP Version: 7.3.21
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -18,21 +18,14 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Cơ sở dữ liệu: `happyshopping`
+-- Database: `shopping`
 --
 
 -- --------------------------------------------------------
 
 --
--- Cấu trúc bảng cho bảng `tbl_account`
+-- Table structure for table `tbl_account`
 --
-DROP TABLE IF EXISTS `tbl_account_otp`;
-CREATE TABLE IF NOT EXISTS `tbl_account_otp` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `username` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
-  `otp` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=20 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 DROP TABLE IF EXISTS `tbl_account`;
 CREATE TABLE IF NOT EXISTS `tbl_account` (
@@ -46,94 +39,28 @@ CREATE TABLE IF NOT EXISTS `tbl_account` (
   `phone` varchar(10) COLLATE utf8_unicode_ci DEFAULT NULL,
   `address` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
   `role` int(11) NOT NULL DEFAULT '1',
-  PRIMARY KEY (`id_user`) 
+  PRIMARY KEY (`id_user`)
 ) ENGINE=MyISAM AUTO_INCREMENT=16 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
-DROP TABLE IF EXISTS `tbl_image_product`;
-CREATE TABLE IF NOT EXISTS `tbl_image_product` (
-  `id_img` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
-  `image` longblob,
-  `id_product` int(11) DEFAULT NULL,
-  PRIMARY KEY (`id_img`)
-) ENGINE=MyISAM AUTO_INCREMENT=7 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+-- --------------------------------------------------------
 
+--
+-- Table structure for table `tbl_account_otp`
+--
+
+DROP TABLE IF EXISTS `tbl_account_otp`;
+CREATE TABLE IF NOT EXISTS `tbl_account_otp` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `otp` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=20 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 -- --------------------------------------------------------
 
 --
--- Cấu trúc bảng cho bảng `tbl_category`
+-- Table structure for table `tbl_cart`
 --
-
-
-DROP TABLE IF EXISTS `tbl_category`;
-CREATE TABLE IF NOT EXISTS `tbl_category` (
-  id_category int(11) NOT NULL AUTO_INCREMENT,
-  name_category varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
-  icon_category varchar(255) COLLATE utf8_unicode_ci NOT NULL DEFAULT 'fas fa-tags',
-  PRIMARY KEY (id_category)
-) ENGINE=MyISAM AUTO_INCREMENT=48 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-
---
--- Đang đổ dữ liệu cho bảng `tbl_category`
---
-
--- --------------------------------------------------------
-
---
--- Cấu trúc bảng cho bảng `tbl_product`
---
-
-DROP TABLE IF EXISTS `tbl_product`;
-CREATE TABLE IF NOT EXISTS `tbl_product` (
-  id_product int(11) NOT NULL AUTO_INCREMENT,
-  name_product varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
-  price decimal(10,0) NOT NULL DEFAULT '0',
-  status tinyint(1) NOT NULL DEFAULT '1',
-  number_liked int(11) NOT NULL DEFAULT '0',
-  quantity int(11) NOT NULL DEFAULT '0',
-  produced_at varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
-  description text COLLATE utf8_unicode_ci,
-  date_created datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  id_category int(11) DEFAULT NULL,
-  PRIMARY KEY (id_product),
-  KEY FK_PRODUCT_CATEGORY (id_category)
-) ENGINE=MyISAM AUTO_INCREMENT=179 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
--- Đang đổ dữ liệu cho bảng `tbl_product`
---
-
--- --------------------------------------------------------
-
---
--- Cấu trúc bảng cho bảng `tbl_product_img`
---
-
-
-DROP TABLE IF EXISTS `tbl_image_product`;
-CREATE TABLE IF NOT EXISTS `tbl_image_product` (
-  id_img int(11) NOT NULL AUTO_INCREMENT,
-  name varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
-  image longblob,
-  number_order int(11) DEFAULT NULL,
-  id_product int(11) DEFAULT NULL,
-  PRIMARY KEY (id_img)
-) ENGINE=MyISAM AUTO_INCREMENT=126 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-
---
--- Đang đổ dữ liệu cho bảng `tbl_product_img`
---
-
-DROP TABLE IF EXISTS `tbl_review`;
-CREATE TABLE IF NOT EXISTS `tbl_review` (
-  id_review int(11) NOT NULL AUTO_INCREMENT,
-  id_user int(11) NOT NULL,
-  id_product int(11) NOT NULL,
-  star int(11) NOT NULL,
-  message varchar(255) COLLATE utf8_unicode_ci NOT NULL,
-  date_review datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY (id_review)
-) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-
 
 DROP TABLE IF EXISTS `tbl_cart`;
 CREATE TABLE IF NOT EXISTS `tbl_cart` (
@@ -142,6 +69,11 @@ CREATE TABLE IF NOT EXISTS `tbl_cart` (
   PRIMARY KEY (`id_cart`)
 ) ENGINE=MyISAM AUTO_INCREMENT=12 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_cart_detail`
+--
 
 DROP TABLE IF EXISTS `tbl_cart_detail`;
 CREATE TABLE IF NOT EXISTS `tbl_cart_detail` (
@@ -152,6 +84,55 @@ CREATE TABLE IF NOT EXISTS `tbl_cart_detail` (
   `price` int(11) NOT NULL,
   PRIMARY KEY (`id_cart_detail`)
 ) ENGINE=MyISAM AUTO_INCREMENT=13 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_category`
+--
+
+DROP TABLE IF EXISTS `tbl_category`;
+CREATE TABLE IF NOT EXISTS `tbl_category` (
+  `id_category` int(11) NOT NULL AUTO_INCREMENT,
+  `name_category` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `icon_category` varchar(255) COLLATE utf8_unicode_ci NOT NULL DEFAULT 'fas fa-tags',
+  PRIMARY KEY (`id_category`)
+) ENGINE=MyISAM AUTO_INCREMENT=48 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_image_product`
+--
+
+DROP TABLE IF EXISTS `tbl_image_product`;
+CREATE TABLE IF NOT EXISTS `tbl_image_product` (
+  `id_img` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `image` longblob,
+  `number_order` int(11) DEFAULT NULL,
+  `id_product` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id_img`)
+) ENGINE=MyISAM AUTO_INCREMENT=126 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_like`
+--
+
+DROP TABLE IF EXISTS `tbl_like`;
+CREATE TABLE IF NOT EXISTS `tbl_like` (
+  `id_user` int(11) NOT NULL,
+  `id_product` int(11) NOT NULL,
+  PRIMARY KEY (`id_user`,`id_product`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_order`
+--
 
 DROP TABLE IF EXISTS `tbl_order`;
 CREATE TABLE IF NOT EXISTS `tbl_order` (
@@ -168,6 +149,12 @@ CREATE TABLE IF NOT EXISTS `tbl_order` (
   PRIMARY KEY (`id_order`)
 ) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_order_detail`
+--
+
 DROP TABLE IF EXISTS `tbl_order_detail`;
 CREATE TABLE IF NOT EXISTS `tbl_order_detail` (
   `id_order_detail` int(11) NOT NULL AUTO_INCREMENT,
@@ -177,6 +164,33 @@ CREATE TABLE IF NOT EXISTS `tbl_order_detail` (
   `price` int(11) NOT NULL,
   PRIMARY KEY (`id_order_detail`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_product`
+--
+
+DROP TABLE IF EXISTS `tbl_product`;
+CREATE TABLE IF NOT EXISTS `tbl_product` (
+  `id_product` int(11) NOT NULL AUTO_INCREMENT,
+  `name_product` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `price` decimal(10,0) NOT NULL DEFAULT '0',
+  `status` tinyint(1) NOT NULL DEFAULT '1',
+  `number_liked` int(11) NOT NULL DEFAULT '0',
+  `quantity` int(11) NOT NULL DEFAULT '0',
+  `produced_at` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `description` text COLLATE utf8_unicode_ci,
+  `date_created` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `id_category` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id_product`)
+) ENGINE=MyISAM AUTO_INCREMENT=179 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_review`
+--
 
 DROP TABLE IF EXISTS `tbl_review`;
 CREATE TABLE IF NOT EXISTS `tbl_review` (
@@ -188,12 +202,8 @@ CREATE TABLE IF NOT EXISTS `tbl_review` (
   `date_review` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id_review`)
 ) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-
-
-
